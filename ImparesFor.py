5c5ead8ca9ca705f2920076901df560499a983c2
Jeyni Lomary <jeynilomary@uamv.edu.ni>
2025-04-09 08:14:51 -0600

    Add files via upload

---
+++ ImparesFor.py
@@ -0,0 +1,31 @@
+numeros = []
+
+for k in range(1, 6): 
+    numero = 2*k - 1
+    numeros.append(numero)
+    
+print(numeros)
+
+
+k = 0
+
+for k in range(5): 
+    numeros.insert(2*k,2*k)
+print(numeros)
+
+
+k = 0
+
+for k in range(5): 
+    numeros.insert(2*k,2*k)
+print(numeros)
+del numeros(2)
+numeros.pop()
+numeros.remove(8)
+print(numeros)
+numeros.append(numero)
+numero = 2
+numeros.insert(2,2)
+print(numeros)
+numeros.remove(2)
+print(numeros)
